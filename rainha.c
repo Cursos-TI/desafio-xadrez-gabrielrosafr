820a2bc93437a65a9b16339d5a074076484cd9bb
gabrielrosafr <gabrielrfernan@gmail.com>
2025-06-04 00:14:01 +0000

    codigo rainha separado

---
+++ rainha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    //declarada a variavel com o nome da peça  do jogo
+    int rainha;
+
+    // atribuiu o valor 1 inicial para a variavel, imprime 8 vezes a direção da rainha utilizando o for, incrementa para nao ocorrer o looping
+    for(rainha = 1; rainha <= 8; rainha++) {
+        printf("Esquerda\n");
+    }
+    
+    return 0;
+}
